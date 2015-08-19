6d378fe794f500ca215526aeb67aea03d5fa1c43
Demon Zhang <demon.zhang@samesamechina.com>
2015-08-19 18:32:54 +0800

    no message

--- protected/views/site/store.php
+++ protected/views/site/store.php
@@ -2,7 +2,9 @@
     <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logo.png" width="100%" class="logo"/>
     
     <div class="store_all">
-        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/store/store_info.png" width="100%" class="store_info"/>
-        <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/store/store_map.jpg" width="100%" class="store_map"/>
+        <p>地址：<span><?php echo $store['address']?></span></p>
+        <p>营业时间：<span><?php echo $store['open']?></span></p>
+        <p>电话：<span><?php echo $store['telphone']?></span></p>
+        <img src="<?php echo $store['mapUrl']; ?>" width="100%" class="store_map"/>
     </div>    
 </div>  
