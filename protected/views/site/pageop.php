bf8b74019cb1dc67a54a5d2ecbc23e22862e10cf
samesameparis <david.peinado@samesameparis.com>
2015-09-23 13:02:46 +0200

    update pageop

--- protected/views/site/pageop.php
+++ protected/views/site/pageop.php
@@ -8,13 +8,19 @@
     </div>
     <div class="text-op">
         <p>
-            优享九九 (包含九月和九折)
+            九月重磅惊喜 - 优享九九！
         </p>
         <p>
-            适逢金色九月，Jerome Dreyfuss迎着浓浓的秋意为微信客户推出独家专属优享，更多惊喜请关注Jerome Dreyfuss官方微信。
+            暑去秋来,浪漫而至。适逢金色九月，为感谢您对我们官方微信的关注和支持, Jerome Dreyfuss 特此推出九月重磅惊喜----微信用户可享独家9折的超值优惠,机会难得,赶快行动吧!
         </p>
-        <p>
-           此活动仅在巴黎以下门店生效:<br/> Galeries Lafayette, rue Jacob, Palais Royal.
+        <p style="text-align:left;">
+        优惠券使用方式:<br/>
+        1. 只需在购买时出示带有"-10%”标志的微信页面，即可享受该优惠。<br/>
+        2. 活动时间为即日起至2015年10月16日止<br/>
+        3. 此活动仅在巴黎Galeries Lafayette, Rue Jacob 和 Palais Royal 三家门店生效<br/>
+        </p>
+        <p style="text-align:left;">
+           更多优惠，更多惊喜，请关注 Jerome Dreyfuss 官方微信账号。
         </p>
     </div>
 
